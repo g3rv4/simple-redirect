2299b487b7c9241673a61d3709c071dace2947ba
Gervasio Marchand <gmc@gmc.uy>
2017-01-20 12:27:37 -0300

    Adding: * Option of running a command after a successful refresh (useful for cache invalidation) * Accepting POSTs (useful for github webhooks) * Cache-breaking url parameter * Ability for the caller to delay the fetch of the file (useful when servers have a delay to serve the new version... ehem, github)

--- server.py
+++ server.py
@@ -3,24 +3,36 @@ import os
 import redis
 import requests
 import yaml
+import subprocess
+import time
 from flask import Flask, request, redirect, abort
 
 app = Flask(__name__)
-redis_cli = redis.StrictRedis(host=os.environ['REDIS_HOST'], db=os.environ['REDIS_DB'])
+redis_cli = None
+
+config_file = os.environ['CONFIG_FILE']
+with open(config_file) as data_file:
+    config = json.load(data_file)
 
 
-@app.route('/refresh')
+def init_redis_cli():
+    global redis_cli
+    if redis_cli is None:
+        redis_cli = redis.StrictRedis(host=os.environ['REDIS_HOST'], db=os.environ['REDIS_DB'])
+
+
+@app.route('/refresh', methods=['GET', 'POST'])
 def refresh():
-    config_file = os.environ['CONFIG_FILE']
-    with open(config_file) as data_file:
-        config = json.load(data_file)
-
     if config['key'] != request.args.get('key'):
         abort(401)
 
+    if request.args.get('wait'):
+        time.sleep(int(request.args.get('wait')))
+
+    init_redis_cli()
     redis_cli.flushdb()
     for domain in config['domains'].keys():
-        domain_file = config['domains'][domain]
+        domain_file = config['domains'][domain] + '?_=%i' % int(round(time.time() * 1000))
 
         try:
             data = requests.get(domain_file).content
@@ -34,12 +46,16 @@ def refresh():
         for slug in data.keys():
             redis_cli.set('url:%s:%s' % (domain, slug), data[slug])
 
+    if os.environ['EXECUTE_AFTER_REFRESH']:
+        subprocess.Popen(os.environ['EXECUTE_AFTER_REFRESH'])
+
     return 'OK'
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    init_redis_cli()
     dest = redis_cli.get('url:%s:%s' % (request.headers['Host'], path))
     if dest:
         return redirect(dest, 301)
